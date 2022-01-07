25cd45276141bcbe78e002c397eda66c9c63fd5b
ahhuang007 <ahhuang007@gmail.com>
2022-01-07 15:10:38 -0500

    finished data_gathering script
    revamped to use datetimes because my function looked terrible, finished the script overall and dealt with logic errors, etc - ready to run now

--- data_gathering.py
+++ data_gathering.py
@@ -12,94 +12,84 @@ data per request, so I had to make a loop to get all the data I wanted.
 
 import requests
 import pandas as pd
+import datetime
 
-def get_date_nums(time_nums):
-    #Function to update the datetime values to next range
-    #Returns 4 ints (format = [month, day, hour_start, hour_end])
-    
-    
-    #Revamp to use datetime library, this is ridiculous
-    month = time_nums[0]
-    day = time_nums[1]
-    hour_start = time_nums[2]
-    hour_end = time_nums[3]
-    #Sorting out the new hours
-    if hour_start > 19:
-        new_hs = 24 - hour_start
-    else:
-        new_hs = hour_start + 5
-    if hour_end > 19:
-        new_he = 24 - hour_end
-    else:
-        new_he = hour_end + 5
-    #Making sure days are updated
-    if month == 2:
-        if day == 28:
-            day = 1
-        else:
-            day += 1
-    return updated_nums
 
-def stringify_nums(time_nums):
+def stringify_nums(d1, d2):
     #Function to convert datetime numbers into proper strings for URL
-    #Returns 4 strings in a list
-    str_li = []
-    for el in time_nums:
-        if el < 10:
-            str_li.append("0" + str(el))
+    #Returns 6 strings in 2 lists (3 in each list)
+    l1 = [d1.month, d1.day, d1.hour]
+    l2 = [d2.month, d2.day, d2.hour]
+    nl1 = []
+    nl2 = []
+    zipped = zip(l1, l2)
+    for el1, el2 in zipped:
+        if el1 < 10:
+            nl1.append("0" + str(el1))
         else:
-            str_li.append(str(el))
-    return str_li
+            nl1.append(str(el1))
+        if el2 < 10:
+            nl2.append("0" + str(el2))
+        else:
+            nl2.append(str(el2))
+    return nl1, nl2
 
-def get_url(str_li, c):
+def get_url(l1, l2, c):
     #Puts datetime values into URL, returns URL string
     p1 = "https://api.pro.coinbase.com/products/" + c + "-USD/candles?"
-    p2 = "start=2021-" + str_li[0] + "-" + str_li[1] + "T" + str_li[2] + ":00:00&"
-    p3 = "end=2021-" + str_li[0] + "-" + str_li[1] + "T" + str_li[3] + ":59:00&granularity=60"
+    p2 = "start=2021-" + l1[0] + "-" + l1[1] + "T" + l1[2] + ":00:00&"
+    p3 = "end=2021-" + l2[0] + "-" + l2[1] + "T" + l2[2] + ":59:00&granularity=60"
     url = p1 + p2 + p3
     return url
 
 headers = {"Accept": "application/json"}
 
-#Creating DataFrames to save later with our data
-dfs = [pd.DataFrame(columns = ['timestamp', 'low', 'high', 'open', 'close', 'volume'])] * 10
+
 #List cryptocurrencies by trade volume:
-    #BTC, ETH, BCH, LINK, SOL, ATOM, MANA, MATIC, DOT, ADA
+#BTC, ETH, BCH, LINK, SOL, ATOM, MANA, MATIC, DOT, ADA
 
 
-cryptos = ['ALGO']#['BTC', 'ETH', 'BCH', 'LINK', 'SOL', 'ATOM', 'MANA', 'MATIC', 'DOT', 'ADA']
+cryptos = ['BTC', 'ETH', 'BCH', 'LINK', 'SOL', 'ATOM', 'MANA', 'MATIC', 'DOT', 'ADA']
 for i in range(len(cryptos)):
     c = cryptos[i]
-    #Starting date/time
-    month = 1
-    day = 1
-    hour_start = 0
-    hour_end = 4
-    time_nums = [month, day, hour_start, hour_end]
+    #Creating DataFrame to save later with our data
+    df = pd.DataFrame(columns = ['timestamp', 'low', 'high', 'open', 'close', 'volume'])
+    
+    #Date/time range
+    date_1 = datetime.datetime(2021, 1, 1)
+    date_2 = date_1 + datetime.timedelta(hours = 4, minutes = 59)
+
     #Converting datetime stuff to strings for url
-    string_list = stringify_nums(time_nums)
+    strl_1, strl_2 = stringify_nums(date_1, date_2)
+    
     #Making URL and request
-    url = get_url(string_list, c)
+    url = get_url(strl_1, strl_2, c)
     response = requests.request("GET", url, headers=headers)
     data = response.json()
+    
     #Add to DataFrame
-    df = pd.DataFrame(data = data, columns = dfs[i].columns)
-    li = [dfs[i], df]
-    dfs[i] = pd.concat(li, ignore_index = True)
-    #while month != 12 and day != 31 and hour != 19:
-    while day != 5 and hour_start != 19: #Test loop
+    subdf = pd.DataFrame(data = data, columns = df.columns)
+    li = [df, subdf]
+    df = pd.concat(li, ignore_index = True)
+    
+    while date_1.month != 12 or date_1.day != 31 or date_1.hour != 19:
+    #while date_1.day != 5 or date_1.hour != 19: #Test loop
         #update at beginning
-        time_nums = get_date_nums(time_nums)
+        date_1 = date_2 + datetime.timedelta(minutes = 1)
+        date_2 = date_1 + datetime.timedelta(hours = 4, minutes = 59)
+        
         #Then proceed with rest of loop
-        string_list = stringify_nums(time_nums)
-        url = get_url(string_list, c)
+        strl_1, strl_2 = stringify_nums(date_1, date_2)
+        url = get_url(strl_1, strl_2, c)
         response = requests.request("GET", url, headers=headers)
         data = response.json()
-        df = pd.DataFrame(data = data, columns = dfs[i].columns)
-        li = [dfs[i], df]
-        dfs[i] = pd.concat(li, ignore_index = True)
-    
-    dfs[i].to_csv("./data/" + c + "_data.csv")
+        subdf = pd.DataFrame(data = data, columns = df.columns)
+        li = [df, subdf]
+        df = pd.concat(li, ignore_index = True)
+        
+    #Reverse rows, save df
+    df = df.iloc[::-1].reset_index(drop = True)
+    df.to_csv("./data/" + c + "_data.csv")
 
     
     
