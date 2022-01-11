a2501092fbeed401eba394dbddb1048ee122ea6d
ahhuang007 <ahhuang007@gmail.com>
2022-01-10 19:11:39 -0500

    completed process script, massive upgrades
    updated process script to be significantly faster at finding missing rows and adding them to data, processing now complete - next step is to resume work on gym env

--- process.py
+++ process.py
@@ -16,27 +16,48 @@ trading occurred.
 import pandas as pd
 import numpy as np
 
+def get_missing_rows(series):
+    #Finds missing row numbers and returns a list of timestamps that are missing
+    nums = []
+    s1 = series[1:].reset_index(drop = True)
+    s2 = series[:len(series) - 1]
+    s3 = s1 - s2
+    #Isolating indices where rows are skipped
+    s4 = s3[s3 != 60]
+    s4ind = list(s4.index)
+    catchup = 0 #Keeps track of missing rows so we can update list appropriately
+    start_time = series[0]
+    for val, ind in zip(s4, s4ind):
+        rows_missing = int((val - 60) / 60)
+        for i in range(rows_missing):
+            nums.append(int(start_time + (ind + 1 + catchup) * 60))
+            catchup += 1
+    
+    return nums
+
 #Our cryptocurrencies
 cryptos = ['AVAX', 'UNI', 'AAVE', 'ALGO', 
            'LTC', 'LRC', 'BTC', 'ETH', 
            'BCH', 'LINK', 'SOL', 'ATOM', 
            'MANA', 'MATIC', 'DOT', 'ADA']
-cryptos = ['ADA']
+
 #Starting and ending time range
 starting_time = 1609459200
 ending_time = 1640995140
 
 for c in cryptos:
     df = pd.read_csv('./data/' + c + '_data.csv')
+    df = df.drop('Unnamed: 0', 1)
+    print(c)
     print(len(df))
-    first_time = df['timestamp'][0] #First recorded time in df
+    first_time = df['timestamp'][0] #First/earliest recorded time in df
     
     #If first_time is after starting_time, then fill in values before first_time
     #with null values
     if first_time > starting_time:
         num_rows = int((first_time - starting_time) / 60) #number of null_rows
         #Creating null dataframe to add
-        timestamps = list(np.linspace(starting_time, first_time, num_rows))
+        timestamps = list(np.linspace(starting_time, first_time - 60, num_rows))
         timestamps = [int(x) for x in timestamps]
         null_col = [0] * num_rows
         null_df = pd.DataFrame(data={'timestamp':timestamps, 'low': null_col, 
@@ -49,26 +70,33 @@ for c in cryptos:
     #Now dealing with periods with no trading activity
     i = first_time + 60
     new_data = []
-    while i != ending_time:
-        print(i)
-        if i not in df['timestamp']: #If row is missing, use data from previous row
-            prev_row_index = df[df['timestamp'] == i - 60].index[0]    
-            prev_row_close = df.loc[prev_row_index]['close']
-            new_row = [i] + [prev_row_close] * 4 + [0]
-            new_row = {'timestamp': i,
-                       'low': prev_row_close,
-                       'high': prev_row_close,
-                       'open': prev_row_close,
-                       'close': prev_row_close,
-                       'volume': 0}
-            df = df.append(new_row, ignore_index = True)
-        i += 60
-        
-    #Add missing rows to df, sort values
-    #new_df = pd.DataFrame(data = new_data, columns = df.columns)
-    #frames = [df, new_df]
+    print(len(df))
+    #Get missing timestamps
+    missing_rows = get_missing_rows(df['timestamp'])
+    
+    #Now add timestamps to dataframe
+    new_rows = []
+    for tstamp in missing_rows:
+        #Check if previous row exists in df
+        prev_row = df[df['timestamp'] == tstamp - 60]
+        if len(prev_row) > 0: #If so, then use that row as data
+            pr_ind = prev_row.index[0]
+            pr_close = df.loc[pr_ind]['close']
+            new_row = [tstamp] + 4 * [pr_close] + [0]
+            new_rows.append(new_row)
+        else: #prev_row must be in new_rows
+            prev_row = [x for x in new_rows if x[0] == tstamp - 60]
+            pr_close = prev_row[0][4]
+            new_row = [tstamp] + 4 * [pr_close] + [0]
+            new_rows.append(new_row)
+            
+    #Concat df to original df, sort values, and save data
+    missing_df = pd.DataFrame(data = new_rows, columns = df.columns)
+    frames = [df, missing_df]
+    df = pd.concat(frames, ignore_index = True)
     df = df.sort_values('timestamp').reset_index(drop = True)
-        
+    print(len(df))
+    df.to_csv('./data/' + c + '_data.csv')
 
         
         
