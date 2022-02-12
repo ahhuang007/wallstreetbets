420fd3f711ca6d8a844ef110e2f6434ac9bb27aa
ahhuang007 <ahhuang007@gmail.com>
2022-02-12 02:21:54 -0500

    added adx indicator
    currently have 3 technical indicators = not putting in RSI since it may be redundant and I don't particularly care for doing this. Updated my data, now can only use row 38+ since beginning has NaN values

--- get_indicators.py
+++ get_indicators.py
@@ -20,7 +20,7 @@ cryptos = ['AAVE', 'ADA', 'ALGO', 'ATOM',
            'AVAX', 'BCH', 'BTC', 'DOT', 
            'ETH', 'LINK', 'LRC', 'LTC', 
            'MANA', 'MATIC', 'SOL', 'UNI']
-cryptos = ["ALGO"]
+#cryptos = ["ALGO"]
 
 for c in cryptos:
     df = pd.read_csv('./data/' + c + '_data.csv')
@@ -46,3 +46,30 @@ for c in cryptos:
     
     
     #Getting ADX
+    interval = 14
+    df['-DM'] = df['low'].shift(1) - df['low']
+    df['+DM'] = df['high'] - df['high'].shift(1)
+    df['+DM'] = np.where((df['+DM'] > df['-DM']) & (df['+DM']>0), df['+DM'], 0.0)
+    df['-DM'] = np.where((df['-DM'] > df['+DM']) & (df['-DM']>0), df['-DM'], 0.0)  
+    df['TR_TMP1'] = df['high'] - df['low']
+    df['TR_TMP2'] = np.abs(df['high'] - df['close'].shift(1))
+    df['TR_TMP3'] = np.abs(df['low'] - df['close'].shift(1))
+    df['TR'] = df[['TR_TMP1', 'TR_TMP2', 'TR_TMP3']].max(axis=1)  
+    df['TR'+str(interval)] = df['TR'].rolling(interval).sum()  
+    df['+DMI'+str(interval)] = df['+DM'].rolling(interval).sum()
+    df['-DMI'+str(interval)] = df['-DM'].rolling(interval).sum()  
+    df['+DI'+str(interval)] = df['+DMI'+str(interval)] /   df['TR'+str(interval)]*100
+    df['-DI'+str(interval)] = df['-DMI'+str(interval)] / df['TR'+str(interval)]*100
+    df['DI'+str(interval)+'-'] = abs(df['+DI'+str(interval)] - df['-DI'+str(interval)])
+    df['DI'+str(interval)] = df['+DI'+str(interval)] + df['-DI'+str(interval)]  
+    df['DX'] = (df['DI'+str(interval)+'-'] / df['DI'+str(interval)])*100  
+    df['ADX'] = df['DX'].rolling(interval).mean() 
+    del df['TR_TMP1'], df['TR_TMP2'], df['TR_TMP3'], df['TR'], df['TR'+str(interval)]
+    del df['+DMI'+str(interval)], df['DI'+str(interval)+'-']
+    del df['DI'+str(interval)], df['-DMI'+str(interval)]
+    del df['+DI'+str(interval)], df['-DI'+str(interval)]
+    del df['DX']
+    del df['-DM']
+    del df['+DM']
+    
+    df.to_csv('./data/' + c + '_data.csv')
