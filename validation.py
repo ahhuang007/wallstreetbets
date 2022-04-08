28b59f22fbe3d4eb74033bdaddab17440e49b549
ahhuang007 <ahhuang007@gmail.com>
2022-04-07 20:13:01 -0400

    tested v37, 38, 39
    Kept training v32, I don't think it's really getting better, especially at validation - going to abandon this current model, maybe increase architecture size again and start anew

--- validation.py
+++ validation.py
@@ -88,7 +88,7 @@ balance_data = [] #Tracks our balance at each timestep
 done = False
 reward = 0
 while not done:
-    action, _states = model.predict(obs, deterministic = False)
+    action, _states = model.predict(obs, deterministic = True)
     obs, rewards, done, info = env.step(action)
     #Appending data to lists
     data.append(rewards)    
